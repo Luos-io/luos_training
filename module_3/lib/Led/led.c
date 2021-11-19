17a89620b72e25c837aa3b6c4e7d60c4eea957b6
nicolas cros <nicolas.cros@luos.io>
2021-11-19 16:02:20 +0100

    add blank project for module 3

---
+++ module_3/lib/Led/led.c
@@ -0,0 +1,50 @@
+/******************************************************************************
+ * @file Led
+ * @brief driver example a simple Led
+ * @author Luos
+ * @version 0.0.0
+ ******************************************************************************/
+#include "led.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+
+/*******************************************************************************
+ * Function
+ ******************************************************************************/
+static void Led_MsgHandler(service_t *service, msg_t *msg);
+
+/******************************************************************************
+ * @brief package init must be call in project init
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Init(void)
+{
+    revision_t revision = {.major = 1, .minor = 0, .build = 0};
+    // Create Led service
+    Luos_CreateService(Led_MsgHandler, STATE_TYPE, "led_service", revision);
+}
+/******************************************************************************
+ * @brief package loop must be call in project loop
+ * @param None
+ * @return None
+ ******************************************************************************/
+void Led_Loop(void)
+{
+}
+/******************************************************************************
+ * @brief Msg manager callback when a msg receive for the led service
+ *        Led can receive messages commands of type COLOR
+ * @param Service destination
+ * @param Msg receive
+ * @return None
+ ******************************************************************************/
+static void Led_MsgHandler(service_t *service, msg_t *msg)
+{
+}
